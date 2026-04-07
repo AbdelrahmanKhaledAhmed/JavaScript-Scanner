b3332077cff00e665bc088f748bd22a9f0c0bf0a
Abdelrahman <boodykhaled943@gmail.com>
2026-04-07 13:56:59 +0200

    Update and rename program.json to modelsConfig.js

---
+++ modelsConfig.js
@@ -0,0 +1,17 @@
+// src/modelsConfig.js
+const models = [
+  {
+    id: "tshirt",
+    name: "T-Shirt",
+    url: "/models/tshirt.glb",
+    scale: 1.6,
+    yOffset: -1.5,
+    zOffset: 0,
+    rotationY: -0.9,
+    cameraPos: [0, 1, 5.5],
+    autoRotate: true,     
+    autoRotateSpeed: 1.5        
+  },
+];
+
+export default models;
